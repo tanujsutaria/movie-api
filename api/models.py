27550db5a1090caa1bf754c38abac8254aa514c7
Tanuj Sutaria <tanuj@Tanujs-MacBook-Pro.local>
2021-10-23 16:28:39 -0700

    Fixing typo in models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from . import db
 
 class Movie(db.Model):
-    id = db.Column(db.Integer, primay_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(50))
-    rating = db.Column(db.Integer )
+    rating = db.Column(db.Integer)
